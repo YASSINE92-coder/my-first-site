e8d34c4242de4900d6bfb7eaf738e8dae95e33ff
yassine chaanoune <yassinechaanoune039@example.com>
2026-07-21 18:41:52 +0100

    Initial commit

---
+++ yassine/hello.java
@@ -0,0 +1,8 @@
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+    public static void printHello() {
+        System.out.println("Hello, World!");
+    }
+}
