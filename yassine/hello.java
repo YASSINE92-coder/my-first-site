679fe7c6dbd24c33570e626a40ec8d8a073471e4
yassine <145224571+YASSINE92-coder@users.noreply.github.com>
2026-07-21 23:23:06 +0100

    Delete yassine directory

--- yassine/hello.java
+++
@@ -1,8 +0,0 @@
-public class hello {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-    public static void printHello() {
-        System.out.println("Hello, World!");
-    }
-}
